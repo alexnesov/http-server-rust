059ef6763827cf927e4319ad590b1f6ea500bdae
alexnesov <alexandrenesovic@gmail.com>
2024-02-28 23:02:54 +0100

    str test

---
+++ target/debug/deps/server-2b48f10ae22e6342.d
@@ -0,0 +1,5 @@
+/home/nesov/Programmation/http-server-rust/target/debug/deps/server-2b48f10ae22e6342: src/main.rs
+
+/home/nesov/Programmation/http-server-rust/target/debug/deps/server-2b48f10ae22e6342.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/server.d
@@ -0,0 +1,1 @@
+/home/nesov/Programmation/http-server-rust/target/debug/server: /home/nesov/Programmation/http-server-rust/src/main.rs
